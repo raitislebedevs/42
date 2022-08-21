d42950bc9cbee72f2836ce10987135debf8f34a2
raitis.lebedevs1 <raitis.lebedevs@gmail.com>
2022-08-21 10:11:35 +0300

    Working on regular expression

--- Node.js/mathAPI/api/simpleMath.js
+++ Node.js/mathAPI/api/simpleMath.js
@@ -5,7 +5,22 @@ const router = express.Router();
 router.post("/", (req, res) => {
   validation(req.body, res);
   let expression = req.body.expression.replace(/\s/g, "");
-  res.send(`Hello you send in an expression as ${expression}`);
+
+  // The pattern search cos(??) in any order there exist pattern (?:cos\((\d+)\)) expresses that the string can contain cos(??).
+  // The same is for the rest. We search for each of the function in the string.
+  // After that we check the result and if the string is identicall the one we selected, then we are good to go with the string.
+
+  const pattern =
+    /(?:cos\((\d+)\))|(?:sin\((\d+)\))|(?:tan\((\d+)\))|(?:cot\((\d+)\))|(?:\d+[.]?\d+)|(?:\+(?![\+\-\*\/]))|(?:\*(?![\+\-\*\/]))|(?:\-(?![\+\-\*\/]))|(?:\/(?![\+\-\*\/]))|(?:\^(?![\+\-\*\/]))$/g;
+
+  let result = expression.match(pattern);
+  if (result.join("") === expression) {
+    console.log("String is valid");
+    res.send(`You have send in a valid string hurray you! ${expression}`);
+    return;
+  }
+
+  res.send(`Hello You send in invalid string :( ==> ${expression}`);
 });
 
 function validation(body, res) {
